961612af837fed5ed9e91dd21017a980e17fa6f6
Marek Sagan <marek.sagan@protonmail.com>
2018-12-05 22:57:32 +0100

    Added the main Spring class

--- BiddingPlatformAPI/src/main/java/logic/App.java
+++ BiddingPlatformAPI/src/main/java/logic/App.java
@@ -1,6 +1,7 @@
 package logic;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
