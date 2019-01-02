d367acd15b657588131c0a5c2f45490f648b81a3
Marek Sagan <marek.sagan@protonmail.com>
2019-01-02 02:30:56 +0100

    Frontend structure redesign

--- BiddingPlatform/src/app/app.module.ts
+++ BiddingPlatform/src/app/app.module.ts
@@ -3,45 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatMenuModule, MatIconModule, MatTreeModule, MatGridListModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
-import {PathsModule} from './paths/paths.module'
 
+import { PathsModule } from './base/paths/paths.module'
 import { CategoriesComponent } from './base/categories/categories.component';
-import { DashbComponent } from './base/dashb/dashb.component';
-import { TreeComponent } from './base/tree/tree.component';
 import { RegisterComponent } from './components/register/register.component';
-import { AddressComponent } from './components/address/address.component';
-
+import { ZorrogridComponent } from './components/zorrogrid/zorrogrid.component';
+import { ZorroregisterComponent } from './components/zorroregister/zorroregister.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ItemsComponent } from './components/items/items.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
     RegisterComponent,
-    DashbComponent,
-    TreeComponent,
-    AddressComponent
+    ZorrogridComponent,
+    ZorroregisterComponent,
+    LayoutComponent,
+    ItemsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatIconModule,
-    MatTreeModule,
-    MatGridListModule,
+    NgZorroAntdModule,
     HttpClientModule,
-    MatCardModule,
-    MatButtonModule,
     LayoutModule,
-    PathsModule
+    PathsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- BiddingPlatform/src/app/base/paths/paths-routing.module.ts
+++ BiddingPlatform/src/app/base/paths/paths-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TreeComponent} from "../base/tree/tree.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'tree', component: TreeComponent }
-];
+  { path: '', redirectTo: '/', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
