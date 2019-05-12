00a5f38005546fe0b38faf1d8a9a9d3d6d32a615
Marek Sagan <marek.sagan@protonmail.com>
2019-05-12 19:19:07 +0200

    Updated some old dependencies

--- frontend/src/app/base/paths/paths-routing.module.ts
+++ frontend/src/app/base/paths/paths-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from '../../components/login/login.component'
-import {ItemsComponent} from "../../components/items/items.component";
-import {ItemComponent} from "../../components/item/item.component";
-import {HomeComponent} from "../home/home.component";
-import {CartComponent} from "../../components/cart/cart.component";
-import {AboutComponent} from "../../components/about/about.component";
-import {AdminComponent} from "../../components/admin/admin.component";
-import {MessagesComponent} from "../../components/messages/messages.component";
-import {SettingsComponent} from "../../components/settings/settings.component";
+import {LoginComponent} from '../../components/login/login.component';
+import {ItemsComponent} from '../../components/items/items.component';
+import {ItemComponent} from '../../components/item/item.component';
+import {HomeComponent} from '../home/home.component';
+import {CartComponent} from '../../components/cart/cart.component';
+import {AboutComponent} from '../../components/about/about.component';
+import {AdminComponent} from '../../components/admin/admin.component';
+import {MessagesComponent} from '../../components/messages/messages.component';
+import {SettingsComponent} from '../../components/settings/settings.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
